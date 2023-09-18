47b86a95d58147d0fa818ee094734516345b52a0
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-18 23:40:03 +0300

    Complete Assignemt

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+ function scuberGreetingForFeet(ride){
   // Write your code here!
+ 
+  if (ride <= 400) {
+      return 'This one is on me!';
+  } 
+  else if (ride > 2000 && ride < 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } 
+  else if (ride > 2500) {
+      return 'No can do.';
+    }
+  }
+function ternaryCheckCity(city){
+  // Write your code here!
+   return city === 'NYC'?'Ok, sounds good.' : "No go."
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+      return "Thank you so much.";
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+      case 'not as generous':
+      return "Thank you.";
+
+      default: 
+      return "Bye.";
+  }
 }
